990cfd1adb78d671d8893966ca2201dd5022ca4f
Luis Almeida <122276206+lra2001@users.noreply.github.com>
2025-10-10 15:21:54 +0100

    add password reset functionality

--- top_clinic/settings.py
+++ top_clinic/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,15 @@ LOGIN_URL = 'login'
 
 MEDIA_ROOT = BASE_DIR / 'media' # Newer modern and Pythonic way
 MEDIA_URL = '/media/'
+
+import certifi
+import ssl
+
+ssl._create_default_https_context = ssl.create_default_context(cafile=certifi.where())
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'lapdfssd@gmail.com' # Login email address, Stored as an environment variable
+EMAIL_HOST_PASSWORD = 'cumh zudo dxyk fpmb'
