3e4698985bd47c2c578bc14266f870e5007164ff
Luis Almeida <122276206+lra2001@users.noreply.github.com>
2025-10-11 11:28:04 +0100

    Add content to pages
    - Remove about page
    - Add content to Specialties and Contact pages
    - Formatted code on all pages

--- top_clinic_app/views.py
+++ top_clinic_app/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-def about(request):
-    return render(request, 'about.html')
-
 def specialties(request):
     return render(request, 'specialties.html')
+
+def contact(request):
+    return render(request, 'contact.html')
